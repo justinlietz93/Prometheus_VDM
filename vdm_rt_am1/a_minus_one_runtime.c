ddf43c561b89af272107aca8429b9bb16289206a
Justin K. Lietz <156801407+justinlietz93@users.noreply.github.com>
2026-03-22 01:33:21 -0500

    Update a_minus_one_runtime.c

--- vdm_rt_am1/a_minus_one_runtime.c
+++ vdm_rt_am1/a_minus_one_runtime.c
@@ -1,49 +1,19 @@
 #include "a_minus_one_runtime.h"
 
-a_minus_one_result a_minus_one_bifop(void **aclass_n,
-                                     const a_minus_one_ops *ops,
-                                     void *ctx) {
-    void *aclass_np1 = 0;
-
-    if (!aclass_n || !ops) {
-        return A_MINUS_ONE_ERROR;
+void *a_minus_one_bifop(void *aclass_n,
+                        const a_minus_one_ops *ops,
+                        void *ctx) {
+    if (ops->artcap_positive(aclass_n, ctx)) {
+        return aclass_n;
     }
 
-    if (!ops->artcap_positive ||
-        !ops->bear ||
-        !ops->discharged ||
-        !ops->orthogonal_admit ||
-        !ops->append_orthogonal) {
-        return A_MINUS_ONE_ERROR;
+    if (ops->bear(aclass_n, ctx) && !ops->discharged(aclass_n, ctx)) {
+        return ops->append_orthogonal(
+            aclass_n,
+            ops->orthogonal_admit(aclass_n, ctx),
+            ctx
+        );
     }
 
-    if (!*aclass_n) {
-        return A_MINUS_ONE_UNDEFINED;
-    }
-
-    if (ops->artcap_positive(*aclass_n, ctx)) {
-        return A_MINUS_ONE_STAY;
-    }
-
-    if (!ops->bear(*aclass_n, ctx)) {
-        return A_MINUS_ONE_UNDEFINED;
-    }
-
-    if (ops->discharged(*aclass_n, ctx)) {
-        return A_MINUS_ONE_UNDEFINED;
-    }
-
-    if (!ops->orthogonal_admit(*aclass_n, &aclass_np1, ctx)) {
-        return A_MINUS_ONE_ERROR;
-    }
-
-    if (!aclass_np1) {
-        return A_MINUS_ONE_ERROR;
-    }
-
-    if (!ops->append_orthogonal(aclass_n, aclass_np1, ctx)) {
-        return A_MINUS_ONE_ERROR;
-    }
-
-    return A_MINUS_ONE_EXTEND;
+    return ops->undefined_case(aclass_n, ctx);
 }
